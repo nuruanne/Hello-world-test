d4a1a207c0d21c0b7854e109f377d2738407a526
nuruanne <varahalu.a@gmail.com>
2026-04-22 13:26:09 +0530

    Added new files and scripts

---
+++ demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This is a demo script."
+echo "It will print some messages to the console."
+echo "current date and time: $(date)"
+echo "Listing files in the current directory:"
+ls -l
+echo "Demo script completed."
